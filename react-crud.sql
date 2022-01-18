5603ceeb135414ff090fda4c2a6aaacd0d0cf06e
Durgesh Sahani <dsahani.dev@gmail.com>
2022-01-18 16:53:57 +0530

    Database schema file added

---
+++ react-crud.sql
@@ -0,0 +1,13 @@
+-- Create database
+CREATE DTAABASE react_crud;
+
+-- Create users table
+CREATE TABLE `react_crud`.`users`
+(
+    `id` int NOT NULL auto_increment,
+    `name` varchar(50),
+    `email` varchar(60),
+    `mobile` bigint(10),
+    `created_at` timestamp,
+    `updated_at` timestamp, PRIMARY KEY (id)
+);
